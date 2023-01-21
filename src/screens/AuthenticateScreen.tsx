d8093e99bac05c7fa3a91dabc5584dacaae39824
Amirabbas rezaei <abbassxz2@gmail.com>
2023-01-22 00:57:05 +0330

    fix

--- src/screens/AuthenticateScreen.tsx
+++ src/screens/AuthenticateScreen.tsx
@@ -1,5 +1,5 @@
 import { StatusBar, Text, View, Image } from "react-native";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import {
   createNativeStackNavigator,
   NativeStackNavigationProp,
@@ -8,12 +8,11 @@ import { useAuthenticateStore } from "../stores/authenticateStore";
 import ZipwaySignIn from "../components/auth/ZipwaySignIn";
 import ZipwayVerifyNumber from "../components/auth/ZipwayVerifyNumber";
 import { useZipwayConfigStore } from "../stores/zipwayConfigStore";
-import { trpc } from "../../utils/trpc";
 import ZipwayNewUser from "../components/auth/ZipwayNewUser";
-
+import { useFocusEffect } from "@react-navigation/native";
 import * as SplashScreen from "expo-splash-screen";
 import { useZipwayConfig } from "../ReactQuery/Mutations";
-
+import {trpc} from "../../utils/trpc"
 type Props = {
   navigation: NativeStackNavigationProp<any, any>;
 };
@@ -22,21 +21,32 @@ const AuthStack = createNativeStackNavigator();
 const AuthenticateScreen = ({ navigation }: Props) => {
   const [appState, setAppState] = useState("LAUNCH");
   const { setAppConfig, appConfig } = useZipwayConfigStore();
-  const {isZipwayConfigSuccess, zipwayConfigData,zipwayConfigFailureReason, isZipwayConfigError, mutateZipwayConfig} = useZipwayConfig()
-
+  const {mutate: mutateAppLog} = trpc.app.log.useMutation()
+  const {
+    isZipwayConfigSuccess,
+    zipwayConfigData,
+    zipwayConfigFailureReason,
+    isZipwayConfigError,
+    mutateZipwayConfig,
+  } = useZipwayConfig();
 
   useEffect(() => {
     if (isZipwayConfigSuccess) {
       setAppConfig(zipwayConfigData);
-      navigation.navigate("MapScreen");
       setAppState("AUTHENTICATED");
-      (async () => {
-        await SplashScreen.hideAsync();
-      })();
+      // (async () => {
+      //   await SplashScreen.hideAsync();
+      // })();
     }
   }, [zipwayConfigData]);
 
   useEffect(() => {
+    if(zipwayConfigFailureReason?.data){
+      mutateAppLog({
+        error: zipwayConfigFailureReason?.data,
+        section: "AuthenticationScreen/zipwayErrorUseEffect"
+      })
+    }
     if (zipwayConfigFailureReason?.data["httpStatus"] === 401) {
       // (async () => {
       //   await SplashScreen.hideAsync();
@@ -45,33 +55,47 @@ const AuthenticateScreen = ({ navigation }: Props) => {
     }
   }, [isZipwayConfigError]);
 
-  useEffect(() => {
-    const unsubscribe = navigation.addListener("focus", () => {
-      mutateZipwayConfig()
-    });
-    // Return the function to unsubscribe from the event so it gets removed on unmount
-    return unsubscribe;
-  }, [navigation]);
+  useFocusEffect(
+    useCallback(() => {
+      mutateZipwayConfig();
+    }, [])
+  );
 
-useEffect(() => {
-  if (appConfig) {
-    // (async () => {
-    //   await SplashScreen.hideAsync();
-    // })();
-    navigation.navigate("MapScreen");
-  }
-},[appConfig])
+  useEffect(() => {
+    if (appConfig) {
+      // (async () => {
+      //   await SplashScreen.hideAsync();
+      // })();
+      mutateAppLog({
+        section: "AuthenticationScreen/appConfigUseEffect",
+        message: "app config loaded and headed to MapScreen"
+      })
+      setAppState("AUTHENTICATED");
+    }
+  }, [appConfig]);
+
+  useEffect(() => {
+    if(appState === "AUTHENTICATED") {
+      navigation.navigate("MapScreen");
+    }
+    if("UNAUTHENTICATED"){
+
+    }
+
+  },[appState])
+
 
   return (
     <>
       <StatusBar backgroundColor={"#fff"} barStyle="dark-content" />
       {appState === "LAUNCH" ? (
         <View className="flex-1 h-full w-full bg-white">
-          <Image
+          {/* <Image
             resizeMode="contain"
             className="flex-1 w-full h-full"
             source={require("../../assets/splash.png")}
-          />
+          /> */}
+          <Text>ZipWay</Text>
         </View>
       ) : appState === "UNAUTHENTICATED" ? (
         <AuthStack.Navigator
